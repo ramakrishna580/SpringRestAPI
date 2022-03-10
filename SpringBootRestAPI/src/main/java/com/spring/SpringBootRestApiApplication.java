5761a8b5acf88374494d08cc2fed4832f74409ee
johnharveysanchez7 <harvex007@gmail.com>
2022-03-10 16:04:43 +0800

    Added comment for new branch

--- SpringBootRestAPI/src/main/java/com/spring/SpringBootRestApiApplication.java
+++ SpringBootRestAPI/src/main/java/com/spring/SpringBootRestApiApplication.java
@@ -8,6 +8,8 @@ public class SpringBootRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApiApplication.class, args);
+		
+		//Test Merge
 	}
 
 }
